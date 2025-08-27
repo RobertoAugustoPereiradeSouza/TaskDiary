c7c9e34fda479799f43ad40a36695dcd3ce9a097
RobertoAugustoPereiraDeSouza <robertoaugustobr123@gmail.com>
2025-08-27 07:57:26 -0300

    atualização conn.php pasta connection

---
+++ backend/connection/conn.php
@@ -0,0 +1,13 @@
+<?php
+$hostname = "localhost";
+$dbname = "taskdiary";
+$username ="root";
+$password = "";
+
+try{
+$conn = new PDO("mysql:host = $hostname;dbname = $dbname", $username, $password);
+$conn > setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo  "A conexão com o banco de dados $dbname foi estabelecida com sucesso!";
+}catch(PDOException $e){
+echo "Erro => ".$e >getMessage();
+}
