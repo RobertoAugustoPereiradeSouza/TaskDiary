ab05681f988117325b709f61aa1e184f346073ef
RobertoAugustoPereiraDeSouza <robertoaugustobr123@gmail.com>
2025-08-29 08:41:31 -0300

    alterações no arquivo conn.php

--- backend/connection/conn.php
+++ backend/connection/conn.php
@@ -6,7 +6,7 @@ $password = "";
 
 try{
 $conn = new PDO("mysql:host = $hostname;dbname = $dbname", $username, $password);
-$conn > setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo  "A conexão com o banco de dados $dbname foi estabelecida com sucesso!";
 }catch(PDOException $e){
 echo "Erro => ".$e >getMessage();
