02c9dfe385c0481320fc56659a7019b670207c6a
RobertoAugustoPereiraDeSouza <robertoaugustobr123@gmail.com>
2025-09-05 08:46:01 -0300

    atualização finalização arquivo create.php

--- backend/model/user/create.php
+++ backend/model/user/create.php
@@ -8,4 +8,27 @@ if (empty($_REQUEST['NAME']) || empty($_REQUEST['EMAIL']) || empty($_REQUEST
 "type" => "error",
 "message" => "Existe(m) campo(s) obrigatório(s) não preenchido(s)."
     );
+} else {
+    try{
+$sql = "INSERT INTO USER (NAME, EMAIL, PASSWORD, LEVEL) VALUES (?, ?, ?, ?)";
+$stmt = $conn ->prepare($sql);// STATEMENT 
+$stmt -> execute([
+$_REQUEST['NAME'],
+$_REQUEST['EMAIL'],
+$_REQUEST['PASSWORD'],
+$_REQUEST['LEVEL']
+]);
+$dados = array(
+    "type" => "success",
+    "message" => "Registro salvo com sucesso!"
+        );
+    } catch (PDOException $e){
+        $dados = array(
+            "type" => "error",
+            "message" => "Erro ao salvar o registro: ".$e ->getMessage()
+                );
+    }
 }
+
+$conn = null;
+echo json_encode($dados);
