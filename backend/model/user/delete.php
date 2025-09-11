9a2cdb95963ad1d14dd685e68c816136d80eed18
RobertoAugustoPereiradeSouza <robertoaugustobr123@gmail.com>
2025-09-11 19:20:44 -0300

    Criar delete.php
    inserção dos arquivos read, update e delete da pasta user

---
+++ backend/model/user/delete.php
@@ -0,0 +1,30 @@
+// colocar dentro da pasta user em model
+<?php
+
+include('../../connection/conn.php');
+
+if (empty($_POST['ID'])){
+    $dados = array(
+"type" => "error",
+"message" => "Existe(m) campo(s) obrigatório(s) não preenchido(s)."
+    );
+ } else {
+       try{
+$sql = "DELETE FROM USER WHERE ID = ?";
+$stmt = $conn ->prepare($sql);// STATEMENT 
+$stmt -> execute([
+$_POST['ID']
+]);
+$dados = array(
+    "type" => "success",
+    "message" => "Registro excluido com sucesso!"
+        );
+    } catch (PDOException $e){
+        $dados = array(
+            "type" => "error",
+            "message" => "Erro ao excluir o registro: ".$e ->getMessage()
+                );
+    }
+ }
+ $conn = null;
+ echo json_encode($dados);
