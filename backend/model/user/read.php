384e1e2995fe1885ef3a7b28a9bda355b3ebcbc5
RobertoAugustoPereiradeSouza <robertoaugustobr123@gmail.com>
2025-09-11 19:17:02 -0300

    Criar read.php
    inserção dos arquivos read, update e delete da pasta user

---
+++ backend/model/user/read.php
@@ -0,0 +1,12 @@
+//colocar dentro da pasta user em model
+
+<?php
+
+include ('../../connection/conn.php');
+
+$sql = "SELECT * FROM USER";
+$stmt = $conn->prepare($sql);
+$stmt -> execute();
+$dados = $stmt->fetchAll(PDO::FETCH_ASSOC);//faz um looping infinito no statement(stmt), gerando todos os registros 
+$conn = null;
+echo json_encode($dados);
